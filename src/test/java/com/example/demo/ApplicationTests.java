82cf06dfc2840d22817ed9f01acd63e9a27bd7e9
Priyanka1900 <pnkmama@gmail.com>
2020-06-09 21:32:39 +0530

    modified file

--- src/test/java/com/example/demo/ApplicationTests.java
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -1,13 +1,15 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootTest
 class ApplicationTests {
-
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) throws Exception {
+		ConfigurableApplicationContext run=SpringApplication.run(Application.class);
+		run.close();
 	}
-
-}
+   }
+ 
